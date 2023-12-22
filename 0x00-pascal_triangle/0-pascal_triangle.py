320475d0e3def5e17428201b9c240cdc66634d08
nja93 <njanjalchege@gmail.com>
2023-12-22 04:11:34 +0300

    alx interview qn

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+this odule returns a list of list of integers
+representing the Pascal's triangle of n.
+"""
+
+
+def pascal_triangle(n):
+    """Pascals triangle of n
+
+    Args:
+      n (int): height of triangle
+
+    Returns:
+      list of lists of integers representing the Pascal’s triangle of n
+    """
+    def factorial(n):
+        if n <= 1:
+            return 1
+        return n * factorial(n - 1)
+
+    def comb(m, n):
+        return factorial(m) // (factorial(n) * factorial(m - n))
+
+    if n <= 0:
+        return []
+
+    return [[comb(i, j) for j in range(i + 1)] for i in range(n)]
