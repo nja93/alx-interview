de104804a63290e904778558e328254f58900663
nja93 <njanjalchege@gmail.com>
2024-01-04 22:37:19 +0300

    alx interview on UTF8

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+Method that determines if a given data set represents
+a valid UTF-8 encoding
+"""
+from typing import List
+
+
+def validUTF8(data: List[int]) -> bool:
+    """
+    """
+    num_bytes = 0
+
+    for byte in data:
+        if num_bytes == 0:
+            if (byte >> 7) == 0b0:
+                continue
+            elif (byte >> 5) == 0b110:
+                num_bytes = 1
+            elif (byte >> 4) == 0b1110:
+                num_bytes = 2
+            elif (byte >> 3) == 0b11110:
+                num_bytes = 3
+            else:
+                return False
+        else:
+            if (byte >> 6) == 0b10:
+                num_bytes -= 1
+            else:
+                return False
+
+    return num_bytes == 0
