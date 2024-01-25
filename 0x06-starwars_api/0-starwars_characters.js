87810b2bffae053cdfc753c10e84fb8256c33f3f
nja93 <njanjal@gmail.com>
2024-01-25 22:27:12 +0300

    fix merge conflict

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,4 +1,4 @@
-#!/usr/bin/node
+#!/usr/bin/env node
 // Write a script that prints all characters of a Star Wars movie:
 // The first argument is the Movie ID - example: 3 = “Return of the Jedi”
 // Display one character name by line in the same order of the list “characters” in the /films/ response
